372f58f2f585776ec01a977fd3d68e08c741bc40
Mopilo <mopahemma@gmail.com>
2018-05-13 17:52:10 +0100

    fix Ui

--- app.js
+++ app.js
@@ -87,10 +87,23 @@ app.get("/blogs/:id/edit", (req, res) => {
 app.put("/blogs/:id", (req, res)=>{
     Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, updatedBlog)=> {
         if(err){
-            res.render("/blogs")
+            res.redirect("/blogs")
         }
         else{
             res.redirect("/blogs/" + req.params.id)
+        }
+    })
+})
+
+
+
+app.delete("/blogs/:id", (req, res)=>{
+    Blog.findByIdAndRemove(req.params.id, (err)=> {
+        if(err){
+            res.redirect("/blogs")
+        }
+        else{
+            res.redirect("/blogs")
         }
     })
 })
